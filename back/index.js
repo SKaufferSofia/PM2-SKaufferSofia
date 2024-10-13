6a9dbfe4141aba80352ddd9cef03fead7942e972
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-10-13 13:19:42 -0300

    updated

--- back/index.js
+++ back/index.js
@@ -1,8 +1,11 @@
 const app = require("./src/server");
 const dbCon = require("./src/config/dbCon");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 dbCon().then((res) => {
-  app.listen(3000, () => {
+  app.listen(process.env.PORT, () => {
     console.log("Servidor escuchando en el puerto 3000");
   }); // esto significa que esta adentro de nuestra pc
 });
